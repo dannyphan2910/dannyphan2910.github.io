105e7ce713c01590e05ce4d3c267edd5e7317c32
Danny Phan <phanhaidang29102000@gmail.com>
2020-03-20 18:29:10 -0400

    some clean up with js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function nav_close() {
 var sectionsClassName = ['about_container', 'project_container', 'resume_container', 'contact_container']
 
 function set(show) {
-    console.log(show);
     document.getElementById(show).style.display = "block";
     var filtered = sectionsClassName.filter(function(value) {
         return value !== show;
@@ -17,4 +16,15 @@ function set(show) {
     for (hide of filtered) {
         document.getElementById(hide).style.display = "none";
     }
+}
+
+function addCard(divId, cardCategory, cardTitle, cardBy, link) {
+    const toAdd =  `<div class="card">
+                        <div class="card_info">
+                            <span class="card_category">${cardCategory}</span>
+                            <h3 class="card_title">${cardTitle}</h3>
+                            <span class="card_by">by <a href="${link || '#'}" class="card_author" title="author">${cardBy}</a></span>
+                        </div>
+                    </div>`;
+    document.getElementById(divId).innerHTML += toAdd;
 }
