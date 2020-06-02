e27afa2f309364cabfb97ea3819406f715f1d297
Danny Phan <phanhaidang29102000@gmail.com>
2020-06-02 11:36:42 +0700

    minor fixes for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -51,6 +51,7 @@ function addCard(imgSrc = 'https://images.pexels.com/photos/2529973/pexels-photo
 // filter cards
 function filter(tag = '') {
     $("div.post").show();
+    $("div.post").removeClass('first-card');
     if (tag != '') {
         // hide elements not with that tag
         $("div.post").not("." + tag).hide();
@@ -59,6 +60,10 @@ function filter(tag = '') {
         $(".project-btns button").hide();
     }
 
+    // first card no margin left
+    $("div.post:visible:eq(0)").addClass('first-card');
+
+    // auto scroll buttons 
     if ($("#project-card-container").get(0).scrollWidth - $("#project-card-container").get(0).clientWidth <= 0) {
         $(".btn-left-icon").hide();
         $(".btn-right-icon").hide();
@@ -105,50 +110,3 @@ window.onclick = function(event) {
       this.closeModal();
     }
 }
-
-// slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-
-
